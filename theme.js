4f1474002247d4099c6c9d0dfb5495a9731b6dc0
HAN <1976877170@qq.com>
2023-12-17 18:53:26 +0800

    文档标题加虚线

--- theme.js
+++ theme.js
@@ -4,6 +4,7 @@ if (idd === 1) {
   const root = document.documentElement;
   root.style.setProperty('--border-is', '2px solid rgb(60, 54, 51)');
   root.style.setProperty('--line-col', 'pink');
+  root.style.setProperty('--border-bottom', '1px dashed #FFF');
 }
 // 如果浅色模式
 if (idd === 0) {
