684105fa1f8e68a849bdd5a33cdcbc5d325e059b
HAN <1976877170@qq.com>
2023-12-17 17:29:02 +0800

    优化调色

--- theme.js
+++ theme.js
@@ -1,10 +1,11 @@
 const idd = window.siyuan.config.appearance.mode;
-
+// 如果是夜间模式
 if (idd === 1) {
   const root = document.documentElement;
-  root.style.setProperty('--border-is', '2px solid tan');
+  root.style.setProperty('--border-is', '2px solid rgb(60, 54, 51)');
+  root.style.setProperty('--line-col', 'pink');
 }
-if (idd === 1) {
-    const root = document.documentElement;
-    root.style.setProperty('--line-col', 'pink');
+// 如果浅色模式
+if (idd === 0) {
+
 }
