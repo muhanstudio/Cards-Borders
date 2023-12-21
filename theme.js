53c3d24e373bd42072289b143e23e3815a6fc1f9
HAN <1976877170@qq.com>
2023-12-21 16:43:41 +0800

    优化线条

--- theme.js
+++ theme.js
@@ -2,7 +2,7 @@ const idd = window.siyuan.config.appearance.mode;
 // 如果是夜间模式
 if (idd === 1) {
   const root = document.documentElement;
-  root.style.setProperty('--border-is', '2px solid rgb(60, 54, 51)');
+  root.style.setProperty('--border-is', '1px solid rgb(60, 54, 51)');
   root.style.setProperty('--line-col', 'pink');
   root.style.setProperty('--border-bottom', '1px dashed #FFF');
   root.style.setProperty('--b3-theme-primary', '#727574');
